20695ddd408fb4a7049d45a10ce2a380de497ba4
Arii-14 <muhammadariusni@gmailcom>
2026-07-13 19:05:32 +0700

    d

--- backend/test_db_insert.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ path: '../.env' });
-const pool = require('./src/config/db');
-pool.query("INSERT INTO events (title, slug, banner, description, category_id, organizer_id, location, maps_link, start_date, end_date, registration_deadline, max_quota, visibility, status) VALUES ('test2', 'test-2', null, 'test desc', null, null, 'loc', null, '2026-07-11T12:00', null, null, 0, 'public', 'draft')")
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => process.exit(0));
