3f835ce457e79e7598707cedbc61e1d7472a929e
Arii-14 <muhammadariusni@gmailcom>
2026-07-13 18:30:27 +0700

    delete

--- backend/src/middlewares/requireAdmin.js
+++
@@ -1,9 +0,0 @@
-const requireAdmin = (req, res, next) => {
-  if (!req.user) return res.status(401).json({ message: 'Tidak terautentikasi.' });
-  if (req.user.role !== 'admin' && req.user.role !== 'super_admin') {
-    return res.status(403).json({ message: 'Akses ditolak. Hanya Admin yang diizinkan.' });
-  }
-  next();
-};
-
-module.exports = requireAdmin;
--- backend/src/middlewares/requireSuperAdmin.js
+++
@@ -1,9 +0,0 @@
-const requireSuperAdmin = (req, res, next) => {
-  if (!req.user) return res.status(401).json({ message: 'Tidak terautentikasi.' });
-  if (req.user.role !== 'super_admin') {
-    return res.status(403).json({ message: 'Akses ditolak. Hanya Super Admin yang diizinkan.' });
-  }
-  next();
-};
-
-module.exports = requireSuperAdmin;
